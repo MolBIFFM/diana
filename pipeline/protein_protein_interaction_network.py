e5e88483f2ba56f66c2214c8953fab1deb6fc947
lucasfein <lucas.fein@stud.uni-frankfurt.de>
2020-11-02 00:36:19 +0100

    restructured

--- pipeline/protein_protein_interaction_network.py
+++ pipeline/protein_protein_interaction_network.py
@@ -1,14 +1,12 @@
 import networkx as nx
 
-class ProteinProteinInteractionNetwork(nx.Graph):
 
+class ProteinProteinInteractionNetwork(nx.Graph):
     def __init__(self):
         super(ProteinProteinInteractionNetwork, self).__init__()
 
     from pipeline.data.excel import add_proteins_from_excel
-    
+
     from pipeline.database.IntAct import add_interactions_from_IntAct
     from pipeline.database.BioGRID import add_interactions_from_BioGRID
     from pipeline.database.STRING import add_interactions_from_STRING
-
-   
