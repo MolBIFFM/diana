8b110318d5170a4b8e49926efcb428e78a17c59b
lucasfein <lucas.fein@stud.uni-frankfurt.de>
2020-12-03 15:20:50 +0100

    sheet columns

--- pipeline/configuration/modify.py
+++ pipeline/configuration/modify.py
@@ -1,6 +1,6 @@
 MODIFY = {
-    "X": lambda x: x,
-    "X.Y;": lambda x: x.split(";")[0].split(".")[0],
-    "Y|X|Y": lambda x: x.split("|")[1],
-    "X_Y": lambda x: x.split("_")[0],
+    "X": lambda entry: entry,
+    "X;Y": lambda entry: entry.split(";")[0],
+    "Y|X|Y": lambda entry: entry.split("|")[1],
+    "X_Y": lambda entry: entry.split("_")[0],
 }
