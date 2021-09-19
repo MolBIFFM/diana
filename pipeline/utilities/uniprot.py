0991252d55bed6808b1d3b00c17391f1d4c97f42
lucasfein <lucas.fein@stud.uni-frankfurt.de>
2021-09-19 13:59:46 +0200

    identifier mapping

--- pipeline/utilities/uniprot.py
+++ pipeline/utilities/uniprot.py
@@ -98,8 +98,6 @@ def get_swissprot_entries():
 
 
 def get_primary_accession(proteins=set()):
-    proteins = set([protein.split("-")[0] for protein in proteins])
-
     primary_accession = {}
     for accessions, _, _, _ in get_swissprot_entries():
         if not accessions or accessions[0] in proteins:
