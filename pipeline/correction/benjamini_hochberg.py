fef42a879e361b463a8dc1b4a13855cfa6de7ddb
lucasfein <lucas.fein@stud.uni-frankfurt.de>
2021-11-24 12:38:11 +0100

    benjamini-hochberg

--- pipeline/correction/benjamini_hochberg.py
+++ pipeline/correction/benjamini_hochberg.py
@@ -1,11 +1,15 @@
 def benjamini_hochberg(p_values):
     # Benjamini-Hochberg p-value adjustment (Benjamini, Heller, Yekutieli (2009))
-    adj_p_values = sorted([list(item) for item in p_values.items()],
-                          key=lambda item: item[1])
-    m = len(adj_p_values)
+    adjusted_p_values = sorted([list(item) for item in p_values.items()],
+                               key=lambda item: item[1])
+    m = len(adjusted_p_values)
 
     for i in range(m):
-        adj_p_values[i][1] = min(
-            min(m * adj_p_values[j][1] / (j + 1) for j in range(i, m)), 1.0)
+        adjusted_p_values[i][1] = min(
+            min(m * adjusted_p_values[j][1] / (j + 1) for j in range(i, m)),
+            1.0)
 
-    return {key: adj_p_value for key, adj_p_value in adj_p_values}
+    return {
+        key: adjusted_p_value
+        for key, adjusted_p_value in adjusted_p_values
+    }
