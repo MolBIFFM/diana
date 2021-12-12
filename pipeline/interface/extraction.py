e9684477d4b1704f72c73fc852972cf521fc47ce
lucasfein <lucas.fein@stud.uni-frankfurt.de>
2021-12-12 13:06:58 +0100

    added sars-cov-2

--- pipeline/interface/extraction.py
+++ pipeline/interface/extraction.py
@@ -1,14 +1,26 @@
+import itertools
+
 EXTRACT = {
     ";":
     lambda entry: entry.split(";"),
     "|;":
-    lambda entry:
-    [e.split("|")[1] for e in entry.rstrip(";").split(";") if "|" in e],
+    lambda entry: [
+        site.split("|")[1] for site in entry.rstrip(";").split(";")
+        if "|" in site
+    ],
     "|":
     lambda entry: [entry.split("|")[1]],
     "_,":
-    lambda entry:
-    [e.split("_")[0] for e in entry.rstrip(",").split(",") if "_" in e],
-    "(":
-    lambda entry: [entry.split("(")[0].rstrip()],
+    lambda entry: [
+        site.split("_")[0] for site in entry.rstrip(",").split(",")
+        if "_" in site
+    ],
+    "[();()]":
+    lambda entry: [
+        position for position in itertools.chain.from_iterable([
+            int(site[1:site.find("(")]) if "(" in site and ")" else None
+            for site in subentry[subentry.find("[") + 1:subentry.find("]")].
+            split(";")
+        ] for subentry in entry.split(";")) if position is not None
+    ]
 }
