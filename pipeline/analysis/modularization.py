9885fdbd939914d1e05cafdca63eb8f0e9306eb3
lucasfein <lucas.fein@stud.uni-frankfurt.de>
2022-04-06 21:50:37 +0200

    modularization performance

--- pipeline/analysis/modularization.py
+++ pipeline/analysis/modularization.py
@@ -25,106 +25,82 @@ def clauset_newman_moore(network: nx.Graph,
         weight: The edge attribute to be utilized as edge weight.
 
     Returns:
-        Communities of the network
+        The communities of the network.
     """
-    A = nx.linalg.graphmatrix.adjacency_matrix(
-        network, weight=weight).toarray().tolist()
-    n = network.number_of_nodes()
-    k = [sum(A[i]) for i in range(n)]
-    m = sum(k) / 2.0
-    a = [k[i] / (2.0 * m) for i in range(n)]
+    communities = {ci: {i} for ci, i in enumerate(network.nodes())}
 
-    communities = [{node} for node in network.nodes()]
-    connected = [[bool(A[i][j]) for j in range(i)] for i in range(n)]
+    A = {
+        i: {j: network.edges[i, j][weight] for j in nx.neighbors(network, i)
+           } for i in network.nodes()
+    }
 
-    delta_q = [[0.0 for _ in range(i)] for i in range(n)]
-    for i in range(n):
-        for j in range(i):
-            if A[i][j]:
-                delta_q[i][j] = 1 / (2.0 * m) - resolution * k[i] * k[j] / (
-                    (2.0 * m)**2.0)
+    connected = {i: set(A[i]) for i in A}
+
+    k = {i: sum(A[i].values()) for i in A}
+    m = sum(k.values()) / 2.0
+
+    delta_q = {
+        i: {
+            j: 1.0 / (2.0 * m) - resolution * k[i] * k[j] /
+            ((2.0 * m)**2.0) if j in A[i] else 0.0 for j in A
+        } for i in A
+    }
+
+    a = {i: k[i] / (2.0 * m) for i in A}
 
     max_entry = -1.0
-    for i in range(n):
-        for j in range(i):
+    for i in delta_q:
+        for j in delta_q[i]:
             if delta_q[i][j] > max_entry:
                 max_entry = delta_q[i][j]
-                max_i = i
-                max_j = j
+                max_i, max_j = i, j
 
     while delta_q[max_i][max_j] > 0.0:
         communities[max_j].update(communities[max_i])
-        communities[max_i] = set()
+        del communities[max_i]
 
-        delta_q_prime = [delta_q[i][:] for i in range(len(delta_q))]
+        delta_q_prime = {
+            i: {j: delta_q[i][j] for j in delta_q[i]} for i in delta_q
+        }
 
-        for k in range(n):
-            if communities[k] and k != max_i and k != max_j:
-                if k < max_i and k < max_j:
-                    if connected[max_i][k] and connected[max_j][k]:
+        for n in delta_q[max_j]:
+            if n != max_i and n != max_j:
+                if n in connected[max_i] or n in connected[max_j]:
+                    if n not in connected[max_i]:
+                        delta_q_prime[max_j][n] = delta_q[max_j][
+                            n] - 2.0 * resolution * a[max_i] * a[n]
+                    elif n not in connected[max_j]:
+                        delta_q_prime[max_j][n] = delta_q[max_i][
+                            n] - 2.0 * resolution * a[max_j] * a[n]
+                    else:
                         delta_q_prime[max_j][
-                            k] = delta_q[max_i][k] + delta_q[max_j][k]
-                    elif connected[max_j][k]:
-                        delta_q_prime[max_j][k] = delta_q[max_j][
-                            k] - 2.0 * resolution * a[max_i] * a[k]
-                    elif connected[max_i][k]:
-                        delta_q_prime[max_j][k] = delta_q[max_i][
-                            k] - 2.0 * resolution * a[max_j] * a[k]
-                    connected[max_j][
-                        k] = connected[max_i][k] or connected[max_j][k]
+                            n] = delta_q[max_i][n] + delta_q[max_j][n]
 
-                elif k < max_i:
-                    if connected[max_i][k] and connected[k][max_j]:
-                        delta_q_prime[k][
-                            max_j] = delta_q[max_i][k] + delta_q[k][max_j]
-                    elif connected[k][max_j]:
-                        delta_q_prime[k][max_j] = delta_q[k][
-                            max_j] - 2.0 * resolution * a[max_i] * a[k]
-                    elif connected[max_i][k]:
-                        delta_q_prime[k][max_j] = delta_q[max_i][
-                            k] - 2.0 * resolution * a[max_j] * a[k]
-                    connected[k][
-                        max_j] = connected[max_i][k] or connected[k][max_j]
+                    delta_q_prime[n][max_j] = delta_q_prime[max_j][n]
 
-                else:
-                    if connected[k][max_i] and connected[k][max_j]:
-                        delta_q_prime[k][
-                            max_j] = delta_q[k][max_i] + delta_q[k][max_i]
-                    elif connected[k][max_j]:
-                        delta_q_prime[k][max_j] = delta_q[k][
-                            max_j] - 2.0 * resolution * a[max_i] * a[k]
-                    elif connected[k][max_i]:
-                        delta_q_prime[k][max_j] = delta_q[k][
-                            max_i] - 2.0 * resolution * a[max_j] * a[k]
-                    connected[k][
-                        max_j] = connected[k][max_i] or connected[k][max_j]
+                    connected[max_j].add(n)
+                    connected[n].add(max_j)
 
         delta_q = delta_q_prime
 
-        for i in range(n):
-            if communities[i]:
-                if i < max_i:
-                    delta_q[max_i][i] = 0.0
-                    connected[max_i][i] = False
+        for i in list(delta_q[max_i]):
+            del delta_q[i][max_i]
+            connected[i].discard(max_i)
 
-                elif i > max_i:
-                    delta_q[i][max_i] = 0.0
-                    connected[i][max_i] = False
+        del connected[max_i]
+        del delta_q[max_i]
 
         a[max_j] += a[max_i]
-        a[max_i] = 0.0
+        del a[max_i]
 
         max_entry = -1.0
-        for i in range(n):
-            if communities[i]:
-                for j in range(i):
-                    if communities[j]:
-                        if delta_q[i][j] > max_entry:
-                            max_entry = delta_q[i][j]
-                            max_i = i
-                            max_j = j
+        for i in delta_q:
+            for j in delta_q[i]:
+                if delta_q[i][j] > max_entry:
+                    max_entry = delta_q[i][j]
+                    max_i, max_j = i, j
 
-    return [community for community in communities if community]
+    return [community for community in communities.values()]
 
 
 def louvain(network: nx.Graph,
@@ -143,50 +119,64 @@ def louvain(network: nx.Graph,
     Args:
         network: An undirected, weighted graph.
         resolution: The resolution parameter for modularity.
-        weight: The edge attribute to be utilized as edge weight.
+        weight: The edge attribute to be used as edge weight.
 
     Returns:
-        Communities of the network.
+        The communities of the network.
     """
-    name = list(network.nodes())
-    communities = [[{i} for i in range(network.number_of_nodes())]]
+    communities = [[{i} for i in network.nodes()]]
 
     community_aggregation = True
 
     while community_aggregation:
         community_aggregation = False
 
-        A = nx.linalg.graphmatrix.adjacency_matrix(
-            network, weight=weight).toarray().tolist()
-        n = network.number_of_nodes()
-        k = [sum(A[i]) for i in range(n)]
-        m = sum(k) / 2.0
+        community = {i: ci for ci, i in enumerate(network.nodes())}
+        communities.append({ci: {i} for ci, i in enumerate(network.nodes())})
 
-        sigma_tot = [sum(A[ci]) for ci in range(n)]
-        sigma_in = [A[ci][ci] for ci in range(n)]
+        A = {
+            i:
+            {j: network.edges[i, j][weight] for j in nx.neighbors(network, i)}
+            for i in network.nodes()
+        }
 
-        k_in = [A[i][:] for i in range(len(A))]
+        sigma_in = {
+            ci: A[i].get(i, 0.0) for ci, i in enumerate(network.nodes())
+        }
+        sigma_tot = {
+            ci: sum(A[i].values()) for ci, i in enumerate(network.nodes())
+        }
 
-        community = list(range(n))
-        communities.append([{i} for i in range(n)])
+        k = {i: sum(A[i].values()) for i in A}
+
+        k_in = {
+            i: {
+                **{
+                    community[i]: 0.0
+                },
+                **{community[j]: A[i][j] for j in A[i]}
+            } for i in A
+        }
+
+        m = sum(k.values()) / 2.0
 
         modularity_optimization = True
 
         while modularity_optimization:
             modularity_optimization = False
 
-            for i in range(n):
-                for l in range(n):
-                    sigma_tot[community[i]] -= A[i][l]
+            for i in A:
+                for n in A[i]:
+                    sigma_tot[community[i]] -= A[i][n]
 
-                    if community[l] == community[i]:
-                        sigma_in[community[i]] -= A[i][l]
+                    if community[n] == community[i]:
+                        sigma_in[community[i]] -= A[i][n]
 
-                k_in[i][community[i]] -= A[i][i]
+                k_in[i][community[i]] -= A[i].get(i, 0.0)
 
                 delta_q = {}
-                for j in range(n):
-                    if A[i][j] and community[i] != community[j]:
+                for j in A[i]:
+                    if community[i] != community[j]:
                         delta_q[j] = (
                             ((sigma_in[community[j]] + k_in[i][community[j]]) /
                              (2.0 * m) - resolution *
@@ -205,13 +195,13 @@ def louvain(network: nx.Graph,
                                (2.0 * m))**2.0 - resolution * (k[i] /
                                                                (2.0 * m))**2.0))
 
-                for l in range(n):
-                    sigma_tot[community[i]] += A[i][l]
+                for n in A[i]:
+                    sigma_tot[community[i]] += A[i][n]
 
-                    if community[l] == community[i]:
-                        sigma_in[community[i]] += A[i][l]
+                    if community[n] == community[i]:
+                        sigma_in[community[i]] += A[i][n]
 
-                k_in[i][community[i]] += A[i][i]
+                k_in[i][community[i]] += A[i].get(i, 0.0)
 
                 if delta_q:
                     max_j = max(delta_q.items(), key=lambda item: item[1])[0]
@@ -220,17 +210,21 @@ def louvain(network: nx.Graph,
                         modularity_optimization = True
                         community_aggregation = True
 
-                        for l in range(n):
-                            sigma_tot[community[i]] -= A[i][l]
-                            sigma_tot[community[max_j]] += A[i][l]
+                        for n in A[i]:
+                            sigma_tot[community[i]] -= A[i][n]
+                            sigma_tot[community[max_j]] += A[i][n]
 
-                            if community[l] == community[i]:
-                                sigma_in[community[i]] -= A[i][l]
-                            elif community[l] == community[max_j]:
-                                sigma_in[community[max_j]] += A[i][l]
+                            if community[n] == community[i]:
+                                sigma_in[community[i]] -= A[i][n]
+                            elif community[n] == community[max_j]:
+                                sigma_in[community[max_j]] += A[i][n]
 
-                            k_in[l][community[i]] -= A[l][i]
-                            k_in[l][community[max_j]] += A[l][i]
+                            k_in[n][community[i]] -= A[n][i]
+
+                            if community[max_j] not in k_in[n]:
+                                k_in[n][community[max_j]] = 0.0
+
+                            k_in[n][community[max_j]] += A[n][i]
 
                         communities[1][community[i]].remove(i)
                         communities[1][community[max_j]].add(i)
@@ -238,47 +232,26 @@ def louvain(network: nx.Graph,
                         community[i] = community[max_j]
 
         if community_aggregation:
-            for ci in range(len(communities[1])):
-                nodes = set()
-                for node in communities[1][ci]:
-                    nodes.update(set(communities[0][node]))
-                communities[1][ci] = nodes
-
+            for ci in communities[1]:
+                if communities[1][ci]:
+                    communities[1][ci] = set.union(
+                        *(communities[0][node] for node in communities[1][ci]))
             communities = communities[1:2]
 
-            weights = [
-                [0.0 for _ in range(i + 1)] for i in range(len(communities[0]))
-            ]
-
-            for i in range(n):
-                weights[community[i]][community[i]] += A[i][i]
-
-                for j in range(i):
-                    if community[j] <= community[i]:
-                        weights[community[i]][community[j]] += A[i][j]
-
-                    if community[j] >= community[i]:
-                        weights[community[j]][community[i]] += A[j][i]
-
-            weights = [[
-                weights[ci][cj] for cj in range(ci + 1) if communities[0][cj]
-            ] for ci in range(len(communities[0])) if communities[0][ci]]
-
-            communities[0] = [
-                community for community in communities[0] if community
-            ]
+            weights = {ci: {} for ci in community.values()}
+            for i in A:
+                for cj in k_in[i]:
+                    if cj not in weights[community[i]]:
+                        weights[community[i]][cj] = 0.0
+                    weights[community[i]][cj] += k_in[i][cj]
 
             network = nx.Graph()
-            network.add_nodes_from(range(len(communities[0])))
-
-            for ci in range(len(communities[0])):
+            for ci in weights:
                 if weights[ci][ci]:
                     network.add_edge(ci, ci, weight=weights[ci][ci])
 
-                for cj in range(ci):
+                for cj in weights[ci]:
                     if weights[ci][cj]:
                         network.add_edge(ci, cj, weight=weights[ci][cj])
 
-    return [
-        set(name[node] for node in community) for community in communities[0]
-    ]
+    return [community for community in communities[0].values() if community]
